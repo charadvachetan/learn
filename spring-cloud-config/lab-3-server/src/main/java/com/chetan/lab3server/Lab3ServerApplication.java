1fbe451bb4ef3941c12c7ad37b646915d7630500
Charadva, Chetan (Contractor) <Chetan_Charadva@comcast.com>
2018-01-14 16:37:55 -0600

    lab 5 added

--- spring-cloud-config/lab-3-server/src/main/java/com/chetan/lab3server/Lab3ServerApplication.java
+++ spring-cloud-config/lab-3-server/src/main/java/com/chetan/lab3server/Lab3ServerApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+
+/**
+ * This is config server.
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class Lab3ServerApplication {
